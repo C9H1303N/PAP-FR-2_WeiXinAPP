edd9dcb002fdb6e53ac50cf6d9b7c38e046ba9fa
姚子健 <18373251@buaa.edu.cn>
2021-04-19 22:10:30 +0800

    添加首页

--- pages/Mainpage/Mainpage.js
+++ pages/Mainpage/Mainpage.js
@@ -1,20 +1,88 @@
 // pages/Mainpage.js
+var app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
+
   data: {
-
+    tabbar: ['推荐','榜单'],
+    winHeight: "", //窗口高度
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
+    searchWord: ""
   },
-
+  navbarTap: function(e){
+    this.setData({
+      currentTab: e.currentTarget.dataset.idx
+    })
+  },
+  searchWordInput: function(e){
+    this.setData({
+      searchWord: e.detail.value
+    })
+  },
+  search: function(e){
+    let word = this.data.searchWord
+    if (word == ''){
+      wx: wx.showToast({
+        title: '请输入关键词'
+      })
+    }
+    this.setData({
+      searchWord: ""
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    let that = this;
+    //  高度自适应
+    wx.getSystemInfo({
+      success: function (res) {
+        let calc = res.windowHeight; //顶部脱离文档流了(- res.windowWidth / 750 * 100);
+        // console.log('==顶部高度==',calc)
+        that.setData({
+          winHeight: calc
+        });
+      }
+    });
   },
-
+  // 滚动切换标签样式
+  switchTab: function (e) {
+    let that = this;
+    // console.log("滚动切换标签",e)
+    that.setData({
+      currentTab: e.detail.current
+    });
+    that.checkCor();
+  },
+  // 点击标题切换当前页时改变样式
+  swichNav: function (e) {
+    let cur = e.currentTarget.dataset.current;
+    if (this.data.currentTab == cur) {
+      return false;
+    } else {
+      this.setData({
+        currentTab: cur
+      })
+    }
+  },
+  //判断当前滚动超过一屏时，设置tab标题滚动条。
+  checkCor: function () {
+    let that = this;
+    if (that.data.currentTab > 3) {
+      that.setData({
+        scrollLeft: 300
+      })
+    } else {
+      that.setData({
+        scrollLeft: 0
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -26,14 +94,16 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.setData({
+      searchWord: ""
+    })
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    
   },
 
   /**
