6951e295cf9924e6bf067a86e6e890b604604855
Ramseyous0109 <18373235@buaa.edu.cn>
2021-04-30 01:52:38 +0800

    430展示

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -19,18 +19,10 @@ Page({
     //console.log(this.post_id)
     var ellipsis = !this.data.ellipsis;
     var contentShow;
-    console.log(postsData)
-    for(var i = 0; i < postsData.length; i++){
-      if(postsData[i].id == this.post_id){
-        data = postsData[i];
-        contentShow = postsData[i].abstract;
-        break;
-      }
-    }
-    console.log(data)
+   contentShow = "我们提出了一个统一的框架来理解来自自中心RGB相机的原始图像序列中的3D手和对象交互。给定一个单一的RGB图像，我们的模型联合估计3D手和物体的姿态，建模它们的交互，并通过一个神经网络的单一前馈识别对象和动作类。我们提出了一个单一的体系结构，它不依赖于外部检测算法，而是在单个图像上进行端到端训练。我们进一步合并和传播信息在时域推断交互手和物体轨迹和识别行动。完整的模型以帧序列作为输入，并输出每帧3D手和物体姿态预测，以及整个序列的对象和动作类别的估计。与处理深度数据和ground-truth注释的方法相比，我们演示了我们算法的最新性能。"
     var maxLength = this.data.maxLength;
     // 如果内容长度少于10，则不截取;否则当处于收起状态，截取7个文字并加上省略号
-    //console.log(contentShow.length)
+    console.log(contentShow.length)
     contentShow = (contentShow.length > maxLength && ellipsis) ? contentShow.substring(0, maxLength - 3) + "..." : contentShow;
     this.setData({
       contentShow: contentShow,
