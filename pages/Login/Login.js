6626a8b67671a9aa8cd60009866ea8ddaefca767
Ramseyous0109 <18373235@buaa.edu.cn>
2021-05-19 11:12:09 +0800

    修改注册

--- pages/Login/Login.js
+++ pages/Login/Login.js
@@ -56,6 +56,10 @@ Page({
         duration: 2000
       })
     } else {
+      wx.switchTab({
+        url: '/pages/Mainpage/Mainpage',
+      })
+      /*
       wx.request({
         url: 'https://pap2.zixfy.com/api/token-auth', 
         method: 'POST',
@@ -82,6 +86,7 @@ Page({
 
         }
       })
+      */
     }
   }
 })
